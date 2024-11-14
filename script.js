69d46a2839e40cd248cd973bc7b62a9c1e46cc1d
NicholasTanz <nicholastanzillo@gmail.com>
2024-11-14 00:13:28 -0500

    basic vexFlow usage

---
+++ script.js
@@ -0,0 +1,23 @@
+const {
+  Renderer,
+  Stave
+} = Vex.Flow;
+
+// Create an SVG renderer and attach it to the DIV element named "boo".
+const div = document.getElementById('output');
+const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+// Configure the rendering context.
+renderer.resize(500, 500);
+const context = renderer.getContext();
+
+// Create a stave of width 400 at position 10, 40 on the canvas.
+const stave = new Stave(10, 40, 1000);
+
+// Add a clef and time signature.
+stave.addClef('treble').addTimeSignature('4/4');
+stave.addClef('bass').addTimeSignature('4/4');
+
+
+// Connect it to the rendering context and draw!
+stave.setContext(context).draw();
